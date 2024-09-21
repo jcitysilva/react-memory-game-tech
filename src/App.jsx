481fcd50829899c3b0ff29f6eb9d853fdf9013d2
jcitysilva <joaosi2010@gmail.com>
2024-09-21 02:16:20 +0100

    corrected cover img, added % and language below the card, minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react';
 import './App.css';
 
 const cardImages = [
-  { src: "/img/bash.png" },
-  { src: "/img/cover.png" },
-  { src: "/img/htmlcss.png" },
-  { src: "/img/javascript.png" },
-  { src: "/img/python.png" },
-  { src: "/img/sql.png" },
-  { src: "/img/ts.png" }
+  { src: "/img/bash.png", name: "Bash", percentage: "33.9%" },
+  { src: "/img/htmlcss.png", name: "HTML/CSS", percentage: "52.9%" },
+  { src: "/img/javascript.png", name: "JavaScript", percentage: "62.3%" },
+  { src: "/img/python.png", name: "Python", percentage: "51%" },
+  { src: "/img/sql.png", name: "SQL", percentage: "51%" },
+  { src: "/img/ts.png", name: "TypeScript", percentage: "38.5%" }
 ];
 
 function App() {
@@ -31,15 +30,23 @@ function App() {
     <div className="App">
       <h1>2024 Most Popular</h1>
       <button onClick={shuffleCards}>New Game</button>
-
-      {/* Conditionally render cards if the array is not empty */}
-      <div className="cards-container">
+  
+      <div className="card-grid">
         {cards.length === 0 ? (
-          <p>Click "New Game" to start!</p>
+          <p className="new-game" alt="new game message">Click "New Game" to start!</p>
         ) : (
           cards.map((card) => (
             <div className="card" key={card.id}>
-              <img src={card.src} alt="card" />
+              <div>
+                <img className="front" src={card.src} alt={card.name} />
+                  <div className="card-info">
+                  <p>{card.name}</p>
+                  <p>{card.percentage}</p>
+                <img className="back" src="/img/cover.png" alt="card back" />
+                <p>% % %</p>
+              </div>
+
+              </div>
             </div>
           ))
         )}
